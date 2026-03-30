c50c49f04bedb4f3e64eb2bdfbb01f0776099a70
Nida Arshad <nidaarshad120@gmail.com>
2026-03-29 23:40:18 -0700

    Added nav-link in the contact us button

--- FrontEnd/JS/nav.js
+++ FrontEnd/JS/nav.js
@@ -3,10 +3,19 @@ const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach(link => {
     const linkPath = new URL(link.href).pathname;
     const activePage = window.location.pathname;
-
     const normalize = (path) => (path === "/" ? "/index.html" : path);
 
     if (normalize(linkPath) === normalize(activePage)) {
-        link.classList.add("border-b-2", "border-[#e06c9f]", "text-[#e06c9f]");
+
+        
+        if (link.textContent.trim() === "Contact Us") {
+            
+            link.classList.remove("bg-[#6e9887]", "text-[#d9e0cb]");
+            link.classList.add("bg-[#efb5ce]", "text-[#582f0e]");
+        } else {
+          
+            link.classList.add("border-b-2", "border-[#e06c9f]", "text-[#e06c9f]");
+        }
+
     }
 });
