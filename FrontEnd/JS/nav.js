a012ee36f107b3988ac0505b004cede92b5c0b67
Nida Arshad <nidaarshad120@gmail.com>
2026-03-30 00:15:34 -0700

    Added filter property

--- FrontEnd/JS/nav.js
+++ FrontEnd/JS/nav.js
@@ -1,21 +1,24 @@
-const navLinks = document.querySelectorAll(".nav-link");
 
+const navLinks = document.querySelectorAll(".nav-link");
 navLinks.forEach(link => {
     const linkPath = new URL(link.href).pathname;
     const activePage = window.location.pathname;
     const normalize = (path) => (path === "/" ? "/index.html" : path);
 
     if (normalize(linkPath) === normalize(activePage)) {
-
-        
         if (link.textContent.trim() === "Contact Us") {
-            
             link.classList.remove("bg-[#6e9887]", "text-[#d9e0cb]");
             link.classList.add("bg-[#efb5ce]", "text-[#582f0e]");
         } else {
-          
             link.classList.add("border-b-2", "border-[#e06c9f]", "text-[#e06c9f]");
         }
-
     }
+});
+
+//  Add to Cart Alert  
+const cartButtons = document.querySelectorAll(".cart-btn");
+cartButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        alert("Hi Sweetie! Your item is added to Cart!");
+    });
 });
