ba50672615001d89582899bc51aeb9237fc94c80
Nida Arshad <nidaarshad120@gmail.com>
2026-04-02 03:46:35 -0700

    Added product table in the database

--- DataBase/BrewPetalCafeSchema.sql
+++ DataBase/BrewPetalCafeSchema.sql
@@ -7,4 +7,14 @@ CREATE TABLE Categories (
 );
 
 --Product Table
-
+CREATE TABLE Products (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    CategoryId  INT NOT NULL,
+    Name NVARCHAR(200) NOT NULL,
+    Description NVARCHAR(500),
+    Price DECIMAL(10,2) NOT NULL,
+    ImageUrl NVARCHAR(300),
+    Stock INT NOT NULL DEFAULT 0,
+ 
+    FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+);
