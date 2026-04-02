f234a0e9b374825951364d5f31024e144296d1bd
Nida Arshad <nidaarshad120@gmail.com>
2026-04-02 06:09:55 -0700

    Added conncetion string in the program.cs

--- BackEnd/BrewPetalCafe.Backend/Program.cs
+++ BackEnd/BrewPetalCafe.Backend/Program.cs
@@ -1,4 +1,5 @@
 using BrewPetalCafe.Backend.Data;
+using BrewPetalCafe.Backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<BrewPetalCafeContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
