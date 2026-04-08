194e152dafb811076f93157bdfad8b79585d473e
Nida Arshad <nidaarshad120@gmail.com>
2026-04-07 22:54:12 -0700

    Added Products, Order and contact controller

---
+++ BackEnd/BrewPetalCafe.Backend/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrewPetalCafe.Backend.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
