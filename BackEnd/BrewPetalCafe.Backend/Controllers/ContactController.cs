3bb75b728a5a589c82cce12317821004832da64c
Nida Arshad <nidaarshad120@gmail.com>
2026-04-09 11:44:18 -0700

    Added api for contact messages

--- BackEnd/BrewPetalCafe.Backend/Controllers/ContactController.cs
+++ BackEnd/BrewPetalCafe.Backend/Controllers/ContactController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using BrewPetalCafe.Backend.Models;
 
 namespace BrewPetalCafe.Backend.Controllers
 {
-    public class ContactController : Controller
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly BrewPetalCafeContext _context;
+
+        public ContactController(BrewPetalCafeContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] ContactMessage message)
+        {
+            message.SentAt = DateTime.Now;
+            _context.ContactMessages.Add(message);
+            _context.SaveChanges();
+            return Ok(new { success = true });
         }
     }
-}
+}S
