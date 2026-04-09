718638765ec2130c1a883cab2063ef4763ed5c58
Nida Arshad <nidaarshad120@gmail.com>
2026-04-09 05:13:12 -0700

    Add Products GET API to fetch all products

--- BackEnd/BrewPetalCafe.Backend/Controllers/ProductsController.cs
+++ BackEnd/BrewPetalCafe.Backend/Controllers/ProductsController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using BrewPetalCafe.Backend.Models;
 namespace BrewPetalCafe.Backend.Controllers
 {
-    public class ProductsController : Controller
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactController : Controller
     {
-        public IActionResult Index()
+        private readonly BrewPetalCafeContext _context;
+        public ContactController(BrewPetalCafeContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        //Getting All products in the database
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
         }
     }
 }
