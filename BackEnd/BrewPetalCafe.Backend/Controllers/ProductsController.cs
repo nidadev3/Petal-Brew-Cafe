cb07e6f14e3eb1f837d510829bb78d87334bcbd7
Nida Arshad <nidaarshad120@gmail.com>
2026-04-09 05:36:39 -0700

    Add API to get products by category

--- BackEnd/BrewPetalCafe.Backend/Controllers/ProductsController.cs
+++ BackEnd/BrewPetalCafe.Backend/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BrewPetalCafe.Backend.Models;
+using Microsoft.EntityFrameworkCore;
 namespace BrewPetalCafe.Backend.Controllers
 {
 
@@ -18,6 +19,15 @@ namespace BrewPetalCafe.Backend.Controllers
         public IActionResult GetAll()
         {
             var products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        //Getting a product by category
+        [HttpGet("by-category/{categoryName}")]
+        public IActionResult GetByCategory(string categoryName)
+        {
+            var products=_context.Products.Include(p=> p.Category)
+                .Where(p=>p.Category.Name == categoryName).ToList();
             return Ok(products);
         }
     }
