1c8d829a4896fe663e54b62c77552c7786f8d419
Nida Arshad <nidaarshad120@gmail.com>
2026-04-10 19:27:53 -0700

    Inserted data into catagory and products table

--- DataBase/BrewPetalCafeSchema.sql
+++ DataBase/BrewPetalCafeSchema.sql
@@ -49,3 +49,17 @@ CREATE TABLE ContactMessages (
     Message NVARCHAR(1000) NOT NULL,
     SentAt  DATETIME NOT NULL DEFAULT GETDATE()
 );
+
+INSERT INTO Categories (Name) VALUES
+('Coffee'), ('Flowers'), ('Bakery');
+
+INSERT INTO Products (CategoryId, Name, Description, Price, ImageUrl, Stock) VALUES
+(1, 'Espresso', 'Strong and rich coffee shot.', 800, 'Images/Expresso.jpg', 50),
+(1, 'Cappuccino', 'Smooth blend of espresso and creamy foam.', 800, 'Images/Cap.jpg', 50),
+(1, 'Latte', 'Mild and creamy coffee.', 800, 'Images/coffee3.jpg', 50),
+(2, 'Rose Bouquet', 'Classic symbol of love.', 800, 'Images/flower1.jpg', 20),
+(2, 'Tulip Basket', 'Elegant and vibrant tulips.', 950, 'Images/flower2.jpg', 20),
+(2, 'Mixed Floral Box', 'Beautiful mix of fresh blooms.', 1200, 'Images/flower3.jpg', 15),
+(3, 'Chocolate Croissant', 'Flaky pastry with rich chocolate.', 450, 'Images/bakery1.jpg', 30),
+(3, 'Cinnamon Roll', 'Soft roll with warm cinnamon and creamy glaze.', 500, 'Images/bakery2.jpg', 30),
+(3, 'Blueberry Muffin', 'Moist muffin with fresh blueberries.', 400, 'Images/bakery3.jpg', 30);
