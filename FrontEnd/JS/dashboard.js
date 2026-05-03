574b6734d1642efd3c945772ff218f1ef89da13f
Nida Arshad <nidaarshad120@gmail.com>
2026-05-03 08:06:22 -0700

    Total orders in dashboard.js

--- FrontEnd/JS/dashboard.js
+++ FrontEnd/JS/dashboard.js
@@ -4,7 +4,10 @@ async function loadDashboard() {
     const productsRes = await fetch("https://localhost:7262/api/products");
     const products = await productsRes.json();
     document.getElementById("totalProducts").textContent = products.length;
-
+ //Toatal Orders
+  const ordersRes = await fetch("https://localhost:7262/api/orders");
+    const orders = await ordersRes.json();
+    document.getElementById("totalOrders").textContent = orders.length;
 
 
 
