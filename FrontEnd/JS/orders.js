62b2dfdc3f3f13446377b0ae633e5a96e8d1d991
Nida Arshad <nidaarshad120@gmail.com>
2026-05-04 12:35:38 -0700

    Added orders table dynamic rendering with status styling

--- FrontEnd/JS/orders.js
+++ FrontEnd/JS/orders.js
@@ -6,4 +6,19 @@ async function loadOrders(){
     if(orders.length==0){
         tbody.innerHTML=`<tr><td colspan="5" class="text-center text-[#e06c9f] py-6">No orders yet!</td></tr>`;
     }
+    orders.forEach(order=>{
+        tbody.innerHTML +=`
+        <tr class= "border-b border-pink-100">
+        <td class="py-3 text-[#6e9887]">#${order.id}</td>
+        <td class="py-3 text-[#6e9887]">${new Date(order.orderDate).toLocaleDateString()}</td>
+        <td class="py-3 text-[#6e9887]">Rs ${order.totalAmount}</td>
+        <td class="py-3">
+        <span class="bg-[#d9e0cb] text-[#582f0e] px-3 py-1 rounded-full text-sm">
+                        ${order.status}
+                    </span>
+                    </td>
+        
+        
+        `
+    })
 }
