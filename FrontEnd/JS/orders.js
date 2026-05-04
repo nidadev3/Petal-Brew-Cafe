05da5423b47a4a0cf0144469c99b67d8353e32f1
Nida Arshad <nidaarshad120@gmail.com>
2026-05-04 11:58:02 -0700

    Added empty state message for orders table

--- FrontEnd/JS/orders.js
+++ FrontEnd/JS/orders.js
@@ -3,4 +3,7 @@ async function loadOrders(){
     const orders =await respsonse.json();
     const tbody=document.getElementById("ordersTable");
     tbody.innerHTML=" ";
+    if(orders.length==0){
+        tbody.innerHTML=`<tr><td colspan="5" class="text-center text-[#e06c9f] py-6">No orders yet!</td></tr>`;
+    }
 }
