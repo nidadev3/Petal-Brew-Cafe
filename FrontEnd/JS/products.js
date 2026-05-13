aee1eb856a6fe90a8cb3336bb35f5d5dd503e4dd
Nida Arshad <nidaarshad120@gmail.com>
2026-05-12 19:49:13 -0700

    Removed the hardcoded data of products table

--- FrontEnd/JS/products.js
+++ FrontEnd/JS/products.js
@@ -1,5 +1,8 @@
 async function loadProducts(){
     const response=await fetch("https://localhost:7262/api/products");
     const products=await response.json();
+
+    const tbody=document.getElementById("productsTable");
+    tbody.innerHTML=" ";
     
 }
